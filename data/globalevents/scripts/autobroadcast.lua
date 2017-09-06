8b8cc211ca55e3e650095799acb26f4a11b061ca
Jesús Rafael Zúñiga Castro <Tatuy@users.noreply.github.com>
2017-09-06 15:29:07 -0700

    Update autobroadcast.lua

--- data/globalevents/scripts/autobroadcast.lua
+++ data/globalevents/scripts/autobroadcast.lua
@@ -1,10 +1,13 @@
--- 
-
 function onThink(interval, lastExecution)
     local MESSAGE = {
-        "[COMMANDS] !buyhouse - !sellhouse - !serverinfo - !online - !uptime - !outfit .",
-        "[BUGS?] REPORTEM EM: http://www.github.com/malucooo/otxserver-new/.",
-		"[CONTATO] FACEBOOK:https://www.facebook.com/erick.nunes.75/ SKYPE: e_nunes@live.com WHATSAPP: +5493412802546.",
+		"[COMMANDS] !buyhouse - !sellhouse - !serverinfo - !online - !uptime - !outfit.",
+		"[TIBIA CLIENT UPDATED] http://uatibia.com/global/ Use our Own Client to avoid debugs and can use all the functions of the server.",
+		"[SECURITY] Do not use the same passwords of others otservers, they can hack your account (use diferent accounts).",
+		"[RULES] http://www.uatibia.com Read the rules to avoid punishments.",
+		"[FACEBOOK] https://www.facebook.com/uatibia",
+		"[TWITTER] https://twitter.com/uatibia",
+		"[PORTAL] http://portal.uatibia.com",
+		"[OPENSOURCE] We are open source: https://github.com/Tatuy/Global",
     }
     Game.broadcastMessage(MESSAGE[math.random(1, #MESSAGE)], MESSAGE_EVENT_ADVANCE) 
     return true
