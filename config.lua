de085b3718ff9e434eb3d7d0db5330b819131efd
Jesús Rafael Zúñiga Castro <Tatuy@users.noreply.github.com>
2018-04-22 16:49:59 -0700

    Update all source data
    4/20.

--- config.lua
+++ config.lua
@@ -8,33 +8,33 @@ removeChargesFromRunes = true
 removeChargesFromPotions = true
 removeWeaponAmmunition = true
 removeWeaponCharges = true
-timeToDecreaseFrags = 12 * 60 * 60 * 1000
+timeToDecreaseFrags = 24 * 60 * 60 * 1000
 whiteSkullTime = 15 * 60 * 1000
 stairJumpExhaustion = 2 * 1000
 experienceByKillingPlayers = false
 expFromPlayersLevelRange = 75
-dayKillsToRedSkull = 5
-weekKillsToRedSkull = 25
-monthKillsToRedSkull = 75
-redSkullDuration = 15
-blackSkullDuration = 23
-orangeSkullDuration = 4
+dayKillsToRedSkull = 3
+weekKillsToRedSkull = 5
+monthKillsToRedSkull = 10
+redSkullDuration = 30
+blackSkullDuration = 45
+orangeSkullDuration = 7
 
 -- Connection Config
 -- NOTE: maxPlayers set to 0 means no limit
-ip = "159.203.78.218"
+ip = "127.0.0.1"
 bindOnlyGlobalAddress = false
 loginProtocolPort = 7171
 gameProtocolPort = 7172
 statusProtocolPort = 7171
 maxPlayers = 0
-motd = "Welcome to Ultima Alianza Tibia Global RPG ONLINE: \n \n www.uatibia.com \n\n Follow us in facebook www.facebook.com/uatibia and twitter www.twitter.com/uatibia and get the official \n source for news, tips and good assistance for the game.\n Join the community in our facebook group: https://www.facebook.com/groups/uatibia/\n\n *Have fun! \n\n\n The Administration."
-onePlayerOnlinePerAccount = false
+motd = "Welcome to The OTXServer Global!"
+onePlayerOnlinePerAccount = true
 allowClones = false
-serverName = "UATibia"
+serverName = "OTXServer-Global"
 statusTimeout = 5 * 1000
 replaceKickOnLogin = true
-maxPacketsPerSecond = 1000
+maxPacketsPerSecond = 25
 
 -- Version Manual
 clientVersionMin = 1100
@@ -85,7 +85,7 @@ maxMarketOffersAtATimePerPlayer = 100
 -- MySQL
 mysqlHost = "127.0.0.1"
 mysqlUser = "root"
-mysqlPass = "tunelsecreto27"
+mysqlPass = ""
 mysqlDatabase = "global"
 mysqlPort = 3306
 mysqlSock = ""
@@ -94,15 +94,15 @@ passwordType = "sha1"
 -- Misc.
 allowChangeOutfit = true
 freePremium = true
-kickIdlePlayerAfterMinutes = 1500
-idleWarningTime = 1500 * 60 * 1000
-idleKickTime = 1500 * 60 * 1000
+kickIdlePlayerAfterMinutes = 15
+idleWarningTime = 10 * 60 * 1000
+idleKickTime = 15 * 60 * 1000
 maxMessageBuffer = 4
 emoteSpells = false
-classicEquipmentSlots = true
+classicEquipmentSlots = false
 allowWalkthrough = true
 coinPacketSize = 1
-coinImagesURL = "http://localhost/global/images/store/"
+coinImagesURL = "http://localhost/images/store/"
 classicAttackSpeed = false
 
 -- Rates
@@ -136,7 +136,7 @@ defaultPriority = "high"
 startupDatabaseOptimization = true
 
 -- Status server information
-ownerName = "UATibia Staff"
-ownerEmail = "contact@uatibia.com"
-url = "http://www.uatibia.com"
-location = "USA"
+ownerName = "Malucooo"
+ownerEmail = "e_nunes@live.com"
+url = "http://www.facebook.com/erick.nunes.75"
+location = "Brazil"
