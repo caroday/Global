9c05b2adb88fd4c59dea915639eb008e2379abcd
Jesús Rafael Zúñiga Castro <Tatuy@users.noreply.github.com>
2018-04-22 18:05:36 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,40 +1,40 @@
 -- Combat settings
 -- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
-worldType = "pvp"
+worldType = "pvp-enforced"
 hotkeyAimbotEnabled = true
-protectionLevel = 7
+protectionLevel = 100
 pzLocked = 60 * 1000
-removeChargesFromRunes = true
+removeChargesFromRunes = false
 removeChargesFromPotions = true
-removeWeaponAmmunition = true
-removeWeaponCharges = true
-timeToDecreaseFrags = 24 * 60 * 60 * 1000
-whiteSkullTime = 15 * 60 * 1000
-stairJumpExhaustion = 2 * 1000
-experienceByKillingPlayers = false
+removeWeaponAmmunition = false
+removeWeaponCharges = false
+timeToDecreaseFrags = 4 * 60 * 60 * 1000
+whiteSkullTime = 7 * 60 * 1000
+stairJumpExhaustion = 500
+experienceByKillingPlayers = true
 expFromPlayersLevelRange = 75
-dayKillsToRedSkull = 3
-weekKillsToRedSkull = 5
-monthKillsToRedSkull = 10
-redSkullDuration = 30
-blackSkullDuration = 45
-orangeSkullDuration = 7
+dayKillsToRedSkull = 5
+weekKillsToRedSkull = 25
+monthKillsToRedSkull = 75
+redSkullDuration = 15
+blackSkullDuration = 22
+orangeSkullDuration = 3
 
 -- Connection Config
 -- NOTE: maxPlayers set to 0 means no limit
-ip = "127.0.0.1"
+ip = "159.203.78.218"
 bindOnlyGlobalAddress = false
 loginProtocolPort = 7171
 gameProtocolPort = 7172
 statusProtocolPort = 7171
 maxPlayers = 0
-motd = "Welcome to The OTXServer Global!"
-onePlayerOnlinePerAccount = true
+motd = "Welcome to Ultima Alianza Tibia Global RPG ONLINE: \n \n www.uatibia.com \n\n Follow us in facebook www.facebook.com/uatibia and twitter www.twitter.com/uatibia and get the official \n source for news, tips and good assistance for the game.\n Join the community in our facebook group: https://www.facebook.com/groups/uatibia/\n\n *Have fun! \n\n\n The Administration."
+onePlayerOnlinePerAccount = false
 allowClones = false
-serverName = "OTXServer-Global"
+serverName = "UATibia"
 statusTimeout = 5 * 1000
 replaceKickOnLogin = true
-maxPacketsPerSecond = 25
+maxPacketsPerSecond = 1000
 
 -- Version Manual
 clientVersionMin = 1100
@@ -64,8 +64,8 @@ deathLosePercent = -1
 
 -- Houses
 -- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
-housePriceEachSQM = 1000
-houseRentPeriod = "daily"
+housePriceEachSQM = 3000
+houseRentPeriod = "never"
 
 -- Item Usage
 timeBetweenActions = 200
@@ -74,7 +74,7 @@ timeBetweenExActions = 1000
 -- Map
 -- NOTE: set mapName WITHOUT .otbm at the end
 mapName = "realmap"
-mapAuthor = "Cipsoft"
+mapAuthor = "UATibia Staff"
 
 -- Market
 marketOfferDuration = 30 * 24 * 60 * 60
@@ -85,7 +85,7 @@ maxMarketOffersAtATimePerPlayer = 100
 -- MySQL
 mysqlHost = "127.0.0.1"
 mysqlUser = "root"
-mysqlPass = ""
+mysqlPass = "tunelsecreto27"
 mysqlDatabase = "global"
 mysqlPort = 3306
 mysqlSock = ""
@@ -94,23 +94,23 @@ passwordType = "sha1"
 -- Misc.
 allowChangeOutfit = true
 freePremium = true
-kickIdlePlayerAfterMinutes = 15
-idleWarningTime = 10 * 60 * 1000
-idleKickTime = 15 * 60 * 1000
+kickIdlePlayerAfterMinutes = 1500
+idleWarningTime = 1500 * 60 * 1000
+idleKickTime = 1500 * 60 * 1000
 maxMessageBuffer = 4
 emoteSpells = false
-classicEquipmentSlots = false
+classicEquipmentSlots = true
 allowWalkthrough = true
 coinPacketSize = 1
-coinImagesURL = "http://localhost/images/store/"
+coinImagesURL = "http://uatibia.com/global/images/store/"
 classicAttackSpeed = false
 
 -- Rates
 -- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
 rateExp = 1
-rateSkill = 50
+rateSkill = 30
 rateLoot = 3
-rateMagic = 25
+rateMagic = 10
 rateSpawn = 1
 
 -- Monster rates
@@ -133,10 +133,10 @@ convertUnsafeScripts = true
 -- NOTE: defaultPriority only works on Windows and sets process
 -- priority, valid values are: "normal", "above-normal", "high"
 defaultPriority = "high"
-startupDatabaseOptimization = true
+startupDatabaseOptimization = false
 
 -- Status server information
-ownerName = "Malucooo"
-ownerEmail = "e_nunes@live.com"
-url = "http://www.facebook.com/erick.nunes.75"
-location = "Brazil"
+ownerName = "UATibia Staff"
+ownerEmail = "contact@uatibia.com"
+url = "http://www.uatibia.com"
+location = "USA"
